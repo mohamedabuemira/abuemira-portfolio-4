8356c28d79b277dcc393a7651657d92f849a83a1
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-06-22 20:29:00 +0300

    update cer

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -5,24 +5,24 @@ export const achievementData = {
             id : 1,
             title : 'odoo certification v 17 & v16',
             details : '-The exam will cover the following subjects: Website, eCommerce, Survey, Marketing, CRM, Sales, Purchases, Project, Timesheet, Accounting, Inventory, MRP, HR, Spreadsheet, POS and Studio.-You’ll have 1.5 hours to complete this 120-questions exam.',
-            date : 'Aug 20, 2019',
+            date : 'May, 2024',
             field : 'ERP',
             image : 'https://raw.githubusercontent.com/mohamedabuemira/abuemira-portfolio-4/main/asset-container/Odoo17FunctionalCertification.jpg'
         },
         {
             id : 2,
-            title : 'AWS Certification',
-            details : 'Validate technical skills and cloud expertise to grow your career and business.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
-            image : 'https://images.unsplash.com/photo-1523474253046-8cd2748b5fd2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            title : 'Project Management',
+            details : ' Creating risk management plans; Understanding process improvement techniques; Managing escalations, team dynamics, and stakeholders; Creating budgets and navigating procurement; Utilizing  project management software, tools, and templates; Practicing Agile project management, with an emphasis on Scrum.Through a mix of videos, assessments, and hands-on activities, you’ll get introduced to initiating, planning, and running both traditional and Agile projects. You’ll develop a toolbox to demonstrate your understanding of key project management elements, including managing a schedule, budget, and tea.',
+            date : 'Aug 20, 2021',
+            field : 'Management',
+            image : 'https://raw.githubusercontent.com/mohamedabuemira/abuemira-portfolio-4/main/asset-container/Odoo17FunctionalCertification.jpg'
         },
         {
             id : 3,
-            title : 'Professional Data Engineer Certification',
-            details : 'Professional Data Engineers enable data-driven decision making by collecting, transforming, and publishing data.',
-            date : 'Aug 20, 2019',
-            field : 'Automation',
+            title : 'Technology entrepreneurship',
+            details : 'This pathway will enable you to launch your own technology startup company. Learn how to form, fund, and launch a new technology venture..',
+            date : 'Aug 20, 2024',
+            field : 'entrepreneurship',
             image : 'https://images.unsplash.com/photo-1489389944381-3471b5b30f04?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
         }
 
