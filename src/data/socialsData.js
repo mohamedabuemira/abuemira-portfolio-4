fa42f411a21e2387656b5f47c39587f984a41af1
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-06-22 19:10:09 +0300

    Update socialsData.js

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -2,7 +2,7 @@ export const socialsData = {
     github: 'https://github.com/mohamedabuemira',
     facebook: 'https://www.facebook.com/mohamed.abuemira',
     linkedIn: 'https://www.linkedin.com/in/abuemira',
-    youtube: 'https://youtube.com/channel/UCRbHttqDCt28Uv7-e18YEjQ'
+    youtube: 'https://youtube.com/channel/UCRbHttqDCt28Uv7-e18YEjQ',
     instagram: 'https://www.instagram.com/',
     codepen: 'https://codepen.io/',
     twitter: 'https://twitter.com/MohamedAbuemira',
@@ -10,6 +10,6 @@ export const socialsData = {
     blogger: 'https://www.blogger.com/',
     medium: 'https://medium.com/@',
     stackOverflow: 'https://stackoverflow.com/users/',
-    gitlab: 'https://gitlab.com/',
-    youtube: 'https://youtube.com/channel/UCRbHttqDCt28Uv7-e18YEjQ'
+    gitlab: 'https://gitlab.com/mohamedabuemira',
+    website: 'https://bento.me/abuemira',
 }
