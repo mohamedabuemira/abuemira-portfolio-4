ce69c66e268039b7c47f4f38cc4331a8c1fb8dec
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-06-22 19:28:29 +0300

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Mohamed Essam Abu Emira',
     title: "ERP Consultant | Product Owner",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://github.com/mohamedabuemira/abuemira-portfolio-4/blob/main/asset-container/landingImg.png',
+    image: 'https://raw.githubusercontent.com/mohamedabuemira/abuemira-portfolio-4/main/asset-container/landingImg.png',
     resumePdf: resume
 }
