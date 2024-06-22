eae5cfa41115b640f8e356772628252973994577
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-06-22 22:09:18 +0300

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
     name: 'Mohamed Essam Abu Emira',
-    title: " Financial Manage|ERP Consultant|Product Owner",
+    title: " Financial Manager| ERP Consultant | Product Owner",
     desciption:"I am an experienced Odoo Techno-Functional Consultant with a proven track record in implementing comprehensive solutions across financial management, supply chain management, sales, CRM, planning, manufacturing, and HRM. My unique blend of technical expertise and business acumen allows me to understand customer needs thoroughly and deliver successful implementations. I specialize in financial management, trade and logistics, discrete and process manufacturing, HR, and real estate. ",
     image: 'https://raw.githubusercontent.com/mohamedabuemira/abuemira-portfolio-4/main/asset-container/landingImg.png',
     resumePdf: resume
