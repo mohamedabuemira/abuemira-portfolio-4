f98abf1382b09293862332c3d057ac340f9544c3
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-06-22 22:01:27 +0300

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,5 +2,5 @@ export const aboutData = {
     title: "Who I am",
     description1: "Hi there, I'm Mohamed Essam Abu Emira. 👋 👋.",
     description2: "Experienced ERP Consultant and Product Owner with a robust background in financial management and ERP systems implementation. Skilled in gathering and documenting business requirements, analyzing and improving business processes, and providing functional specifications for ERP systems. Proficient in collaborating with development teams, conducting user acceptance testing, training end-users, and offering ongoing support. Demonstrates strong project management abilities, ensuring that ERP systems meet organizational needs and deliver value to stakeholders. .",
-    image: orangeThemeboy
+    image: 3
 }
