71c7182521ed19223663e848550f2cbebb256c5a
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-06-23 14:09:43 +0300

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,18 +1,18 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Accounting,Finance,SupplyChain,Auditing,ERP,HR',
-        course: 'Cairo University-Bachelor of Commerce',
+        institution: 'Cairo University',
+        course: 'Bachelor of Commerce,Accounting&Finance',
         startYear: '2008',
         endYear: '2012'
     },
-//    {
+//    {  ,
 //        id: 2,
 //        institution: ' High School, Kidasa, Egypt',
 //        course: 'GPA: 77% ',
 //        startYear: '2005',
 //        endYear: '2008'
-//    },    'Cairo University-Bachelor of Commerce',
+//    },
 //    {
 //        id: 3,
 //        institution: 'Hawking University, Cambridge',
