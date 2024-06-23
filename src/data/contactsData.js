21f91ae5c46b6f892fcf94bd535c1f16d0b91b76
Mohamed Essam AbuEmira <54208500+mohamedabuemira@users.noreply.github.com>
2024-06-23 10:55:34 +0300

    Update contactsData.js

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'abuemira91@gmail.com',
     phone: '+201005061349',
-    address: 'Kirdasa, Giza, Egypt  ',
+    address: 'Kirdasa, Giza, Egypt ',
 
-    sheetAPI: 'https://sheetdb.io/api/v1/k1tb0pkfs4ieo'
+    sheetAPI: 'k1tb0pkfs4ieo'
 }
